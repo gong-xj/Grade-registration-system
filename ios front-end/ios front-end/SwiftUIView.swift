5ea36f2337b95592d1d665b20727eefd39fdeb29
gong-xj <kyoukinketsu@gmail.com>
2020-07-01 21:18:36 +0900

    （忽视：if else示例 ）

---
+++ ios front-end/ios front-end/SwiftUIView.swift
@@ -0,0 +1,34 @@
+////
+////  SwiftUIView.swift
+////  ios front-end
+////
+////  Created by Gongxinjie on 2020/06/30.
+////  Copyright © 2020 Gongxinjie. All rights reserved.
+////
+//
+//import SwiftUI
+//
+//struct SwiftUIView: View {
+//     @State var a = 0
+//
+//    var body: some View {
+//        VStack {
+//            if a >= 3 {
+//                Text("1")
+//            } else {
+//                Text("2")
+//                Button(action: {
+//                    self.a = self.a + 1
+//                }) {
+//                    Text("按钮")
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//struct SwiftUIView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SwiftUIView()
+//    }
+//}
